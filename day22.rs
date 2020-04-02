adc2649782b184c64aa676759040b46a97f0130e
Tsung-Ju Lii <usefulalgorithm@gmail.com>
2020-04-02 12:26:26 +0800

    22
    This one is tricky... modulo operation in Rust would NOT change the sign
    of the number, if the number gets casted to unsigned then the result
    would be incorrect. I manually added `cards` to the modulo result, but I
    think there's probably a function that lets me do exactly this...
    
    Signed-off-by: Tsung-Ju Lii <usefulalgorithm@gmail.com>

--- day22.rs
+++ day22.rs
@@ -1,12 +1,12 @@
+use std::collections::VecDeque;
 use std::env;
 use std::fs;
-use std::collections::VecDeque;
 
 #[derive(Debug, Eq, PartialEq, Clone, Copy)]
 enum Action {
-    Cut(i32),
+    Cut(i128),
     New,
-    Inc(usize),
+    Inc(u128),
 }
 
 impl Action {
@@ -14,10 +14,14 @@ impl Action {
         if s.contains("deal into new stack") {
             Action::New
         } else if s.contains("deal with increment ") {
-            let n = s.split_at("deal with increment ".len()).1.parse::<usize>().unwrap();
+            let n = s
+                .split_at("deal with increment ".len())
+                .1
+                .parse::<u128>()
+                .unwrap();
             Action::Inc(n)
         } else if s.contains("cut ") {
-            let n = s.split_at("cut ".len()).1.parse::<i32>().unwrap();
+            let n = s.split_at("cut ".len()).1.parse::<i128>().unwrap();
             Action::Cut(n)
         } else {
             panic!("Unknown action: {}", s);
@@ -26,9 +30,9 @@ impl Action {
 }
 
 fn first() -> u128 {
-    let input = fs::read_to_string(env::args().nth(1).unwrap()).unwrap();
-    let mut cards = (0..env::args().nth(2).unwrap().parse::<u64>().unwrap()).collect::<Vec<_>>();
-    let which = env::args().nth(3).unwrap().parse::<u64>().unwrap();
+    let input = fs::read_to_string(env::args().nth(2).unwrap()).unwrap();
+    let mut cards = (0..env::args().nth(3).unwrap().parse::<u64>().unwrap()).collect::<Vec<_>>();
+    let which = env::args().nth(4).unwrap().parse::<u64>().unwrap();
     for line in input.trim().lines() {
         match Action::parse(line) {
             Action::New => cards = cards.into_iter().rev().collect(),
@@ -37,13 +41,13 @@ fn first() -> u128 {
                 let mut i = 0;
                 while let Some(x) = temp.pop_front() {
                     cards[i] = x;
-                    i = (i + n) % cards.len();
+                    i = (i + n as usize) % cards.len();
                 }
             }
             Action::Cut(n) => {
                 let at = match n >= 0 {
                     true => n,
-                    false => (cards.len() as i32 + n),
+                    false => (cards.len() as i128 + n),
                 } as usize;
                 let mut temp = cards.split_off(at);
                 temp.append(&mut cards);
@@ -51,15 +55,71 @@ fn first() -> u128 {
             }
         }
     }
-    cards.into_iter().enumerate().find(|(_, v)| *v == which).unwrap().0 as u128
+    cards
+        .into_iter()
+        .enumerate()
+        .find(|(_, v)| *v == which)
+        .unwrap()
+        .0 as u128
+}
+
+fn mod_pow(mut base: u128, mut exp: u128, modulus: u128) -> u128 {
+    if modulus == 1 {
+        return 0;
+    }
+    let mut result = 1;
+    base = base % modulus;
+    while exp > 0 {
+        if exp % 2 == 1 {
+            result = result * base % modulus;
+        }
+        exp = exp >> 1;
+        base = base * base % modulus
+    }
+    result
 }
 
 fn second() -> u128 {
-    0
+    let cards: i128 = 119315717514047;
+    let target = 2020;
+    let times: u128 = 101741582076661;
+    let input = fs::read_to_string(env::args().nth(2).unwrap()).unwrap();
+    let (mut offset_diff, mut increment_mul) = (0i128, 1i128);
+    let inv = |i| mod_pow(i, cards as u128 - 2, cards as u128) as i128;
+    for line in input.trim().lines() {
+        match Action::parse(line) {
+            Action::New => {
+                increment_mul *= -1;
+                increment_mul %= cards;
+                offset_diff += increment_mul;
+                offset_diff %= cards;
+            }
+            Action::Inc(n) => {
+                increment_mul *= inv(n);
+                increment_mul %= cards;
+            }
+            Action::Cut(n) => {
+                offset_diff += increment_mul * n as i128;
+                offset_diff %= cards;
+            }
+        }
+    }
+    increment_mul += cards;
+    offset_diff += cards;
+    let increment = mod_pow(increment_mul as u128, times, cards as u128);
+    let mut offset = offset_diff * (1 - increment as i128) % cards;
+    offset *= inv((((1 - increment_mul) % cards) + cards) as u128) % cards;
+    offset %= cards;
+    offset += cards;
+    (offset as u128+ target * increment) % cards as u128
 }
 
 fn main() {
-    let part = env::args().nth(4).unwrap_or("1".to_string()).parse::<usize>().unwrap();
+    let part = env::args()
+        .nth(1)
+        .unwrap_or("1".to_string())
+        .parse::<usize>()
+        .unwrap();
     let res = match part {
         1 => first(),
         _ => second(),
